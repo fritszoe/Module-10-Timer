3d2e5ccd97648428f7c8e3fc6c3f60b38f4b4f23
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2024-05-07 14:07:39 +0700

    Experiment 1.2: Understanding how it works.

--- src/main.rs
+++ src/main.rs
@@ -105,6 +105,7 @@ fn main() {
         println!("Fatih's Laptop: done!");
     });
 
+    println!("Fatih's Laptop: Yahallo!");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
     drop(spawner);
